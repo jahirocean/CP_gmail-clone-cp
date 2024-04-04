42a0c70d26671ec351876cf0984ffe32925933d6
Zahir Raihan <zahirraihann@gmail.com>
2024-04-04 13:53:30 +0600

    first commit

--- src/components/data/SidebarButtonItems.js
+++ src/components/data/SidebarButtonItems.js
@@ -1,11 +1,12 @@
-
 import StarIcon from "@material-ui/icons/Star";
 import WatchLaterIcon from "@material-ui/icons/WatchLater";
 import SendIcon from "@material-ui/icons/Send";
 import DraftsIcon from "@material-ui/icons/Drafts";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import InboxIcon from "@material-ui/icons/Inbox";
 
 export const SidebarButtonItems = [
+  { icon: <InboxIcon />, title: "Inbox" },
   {
     icon: <StarIcon />,
     title: "Starred",
